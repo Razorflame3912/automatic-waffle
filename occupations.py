5f8b056c9c22c364848b2e90b4fa93d43cf122c4
Mansour <melsharawy@stuy.edu>
2017-09-14 19:07:12 -0400

    Converted str to nested list, took care of quote-comma issue

--- occupations.py
+++ occupations.py
@@ -3,6 +3,37 @@ import random
 f = open('occupations.csv','r')
 
 str = f.read()
-li = str.split('\r\n')
-print li
+li = str.split('\n')
+#print li
+#print len(li)
+linested = []
+for element in li:
+    linested.append(element.split(','))
+#print linested
+linested.remove(linested[len(linested)-1])
+
+index1 = 0
+while index1 < len(linested):
+    current = linested[index1]
+    print current[0]
+    if current[0][0] == '"':
+        while len(current) > 2:
+            current[0] = current[0].replace('"','') + ',' + current[1].replace('"','')
+            current.remove(current[1])
+    index1 += 1
+
+print linested
+
+
+
     
+        
+
+
+'''
+index = 1
+dic = {}
+while index < len(liheader):
+    
+    index += 1
+'''
